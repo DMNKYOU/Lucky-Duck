d76c02d48e6d1ea4161f57f481282507fd76a7b7
Dmitriy Zalesin <87304031+DMNKYOU@users.noreply.github.com>
2021-10-07 23:35:41 +0300

    add Mail and Lottery part

---
+++ LuckyDucky.MailService/EmailService.cs
@@ -0,0 +1,67 @@
+﻿using LuckyDucky.MailService.Interfaces;
+using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace LuckyDucky.MailService {
+    public class EmailService : IMailService {
+
+        private readonly EmailSettingsModel _emailSettings;
+
+        public EmailService(EmailSettingsModel model)
+        {
+            _emailSettings = model;
+        }
+
+        private async Task SenderAsync(MimeMessage emailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, ch, e) => true;
+                await client.ConnectAsync(_emailSettings.MailServer, _emailSettings.MailPort, false);
+                await client.AuthenticateAsync(_emailSettings.Sender, _emailSettings.Password);
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+
+        public async Task SendHtmlEmail(string[] recipients, string title, string htmlBody)
+        {
+
+            foreach (var email in recipients)
+            {
+                var emailMessage = new MimeMessage();
+
+                emailMessage.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.Sender));
+                emailMessage.To.Add(new MailboxAddress("", email));
+                emailMessage.Subject = title;
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = htmlBody
+                };
+
+                await SenderAsync(emailMessage);
+            }
+        }
+
+        public async Task SendPlainTextEmail(string[] recipients, string title, string plainTextBody)
+        {
+
+            foreach (var email in recipients)
+            {
+                var emailMessage = new MimeMessage();
+
+                emailMessage.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.Sender));
+                emailMessage.To.Add(new MailboxAddress("", email));
+                emailMessage.Subject = title;
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+                {
+                    Text = plainTextBody
+                };
+
+                await SenderAsync(emailMessage);
+            }
+        }
+    }
+}
---
+++ LuckyDucky.MailService/Interfaces/IMailService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace LuckyDucky.MailService.Interfaces
+{
+    public interface IMailService
+    {
+        Task SendHtmlEmail(string[] recipients, string title, string htmlBody);
+
+        Task SendPlainTextEmail(string[] recipients, string title, string plainTextBody);
+    }
+}
